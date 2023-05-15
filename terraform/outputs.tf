ae0b3849a3e8fd22f154abd790c2b7a50ced8349
alexandre <70174844+alexkodingu@users.noreply.github.com>
2023-05-15 09:24:03 +0100

    adding helm deployment+gke

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,8 @@ output "docker_repository"{
     description = "docker registry from gcp artifact"
     value       = local.repo_address
 }
+
+output "cluster_name"{
+    description = "GKE cluster name"
+    value       = google_container_cluster.my_cluster.name
+}
