49f1053f3182b85c6f3638b4c374ba049fbc921a
alexandre <70174844+alexkodingu@users.noreply.github.com>
2023-05-15 09:46:51 +0100

    trying some

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -10,6 +10,7 @@ resource "google_container_cluster" "my_cluster" {
     enable_components = []
   }
   monitoring_service = "none"
+  logging_service = "none"
   
   network    = google_compute_network.vpc.name
   subnetwork = google_compute_subnetwork.subnet.name
@@ -24,16 +25,10 @@ resource "google_container_node_pool" "my_nodes" {
   location = var.gke_location
 
   node_config {
-#    oauth_scopes = [
-#      "https://www.googleapis.com/auth/logging.write",
-#      "https://www.googleapis.com/auth/monitoring",
-#    ]
 
     labels = {
       env = var.GCP_PROJECT_ID
     }
-
-#    service_account = data.google_service_account.default.email
 
     machine_type = var.gke_node_type
     disk_size_gb = var.gke_node_disk
